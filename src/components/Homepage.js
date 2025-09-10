36caf83c9210ca2ec2b1faaa42b75221a8420ccc
Pierre Minet <pierre.minet@outlook.fr>
2025-09-10 18:00:02 +0200

    Add base of CSS

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+function Homepage() {
+    return (
+        <>
+            <div>
+            </div>
+            <article>
+                <h1>My Online</h1>
+                <h2>Grocery App</h2>
+            </article>
+        </>
+    );
+}
+
+export default Homepage;
